bb70d4f0303793dd57645dadf57e40c61cb4faaf
Timur Duehr <tduehr@gmail.com>
2018-05-08 18:21:38 -0500

    cosmosdb

--- support/cas-server-support-cosmosdb-service-registry/src/test/java/org/apereo/cas/services/CosmosDbServiceRegistryTests.java
+++ support/cas-server-support-cosmosdb-service-registry/src/test/java/org/apereo/cas/services/CosmosDbServiceRegistryTests.java
@@ -34,7 +34,7 @@ import static org.junit.Assert.*;
 @IfProfileValue(name = "cosmosDbEnabled", value = "true")
 @TestPropertySource(locations = {"classpath:/cosmosdb.properties"})
 @Slf4j
-public class CosmosDbServiceRegistryTests {
+public class CosmosDbServiceRegistryTests extends AbstractServiceRegistryTests {
 
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
@@ -46,34 +46,8 @@ public class CosmosDbServiceRegistryTests {
     @Qualifier("cosmosDbServiceRegistry")
     private ServiceRegistry serviceRegistry;
 
-    private void deleteAll() {
-        final List<RegisteredService> services = this.serviceRegistry.load();
-        services.forEach(service -> this.serviceRegistry.delete(service));
-    }
-
-    @Test
-    public void verifySaveAndLoad() {
-        deleteAll();
-        assertTrue(this.serviceRegistry.load().isEmpty());
-        assertTrue(this.serviceRegistry.size() == 0);
-        
-        final List<RegisteredService> list = new ArrayList<>();
-        IntStream.range(0, 5).forEach(i -> {
-            list.add(buildService(i));
-            this.serviceRegistry.save(list.get(i));
-        });
-        final List<RegisteredService> results = this.serviceRegistry.load();
-        assertEquals(results.size(), list.size());
-        results.forEach(r -> {
-            final RegisteredService s1 = this.serviceRegistry.findServiceById(r.getId());
-            final RegisteredService s2 = this.serviceRegistry.findServiceById(r.getServiceId());
-            assertEquals(s1, s2);
-        });
-        deleteAll();
-        assertTrue(this.serviceRegistry.load().isEmpty());
-    }
-
-    private static RegisteredService buildService(final int i) {
-        return RegisteredServiceTestUtils.getRegisteredService("^http://www.serviceid" + i + ".org");
+    @Override
+    public ServiceRegistry getNewServiceRegistry() {
+        return serviceRegistry;
     }
 }
