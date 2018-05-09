b193c00dde7f1e1b5833304c4a89e5704f9e50cd
Timur Duehr <tduehr@gmail.com>
2018-05-08 21:19:45 -0500

    mongo

--- support/cas-server-support-mongo-service-registry/src/test/java/org/apereo/cas/services/MongoServiceRegistryCloudTests.java
+++ support/cas-server-support-mongo-service-registry/src/test/java/org/apereo/cas/services/MongoServiceRegistryCloudTests.java
@@ -15,13 +15,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 
 import static org.junit.Assert.*;
@@ -36,7 +29,7 @@ import static org.junit.Assert.*;
 @SpringBootTest(classes = {MongoDbServiceRegistryConfiguration.class, RefreshAutoConfiguration.class})
 @TestPropertySource(locations = {"classpath:/mongoservices.properties"})
 @Slf4j
-public class MongoServiceRegistryCloudTests {
+public class MongoServiceRegistryCloudTests extends AbstractServiceRegistryTests {
 
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
@@ -49,44 +42,8 @@ public class MongoServiceRegistryCloudTests {
     private ServiceRegistry serviceRegistry;
 
 
-    @Before
-    public void clean() {
-        final List<RegisteredService> services = this.serviceRegistry.load();
-        services.forEach(service -> this.serviceRegistry.delete(service));
-    }
-
-    @Test
-    public void verifySaveAndLoad() {
-        final List<RegisteredService> list = new ArrayList<>();
-        IntStream.range(0, 5).forEach(i -> {
-            list.add(buildService(i));
-            this.serviceRegistry.save(list.get(i));
-        });
-        final List<RegisteredService> results = this.serviceRegistry.load();
-        assertEquals(results.size(), list.size());
-        IntStream.range(0, 5).forEach(i -> assertEquals(list.get(i), results.get(i)));
-        IntStream.range(0, 5).forEach(i -> this.serviceRegistry.delete(results.get(i)));
-        assertTrue(this.serviceRegistry.load().isEmpty());
-    }
-
-    @After
-    public void after() {
-        clean();
-    }
-
-    private static RegisteredService buildService(final int i) {
-        final AbstractRegisteredService rs = RegisteredServiceTestUtils.getRegisteredService("^http://www.serviceid" + i + ".org");
-
-        final Map<String, RegisteredServiceProperty> propertyMap = new HashMap<>();
-        final DefaultRegisteredServiceProperty property = new DefaultRegisteredServiceProperty();
-        final Set<String> values = new HashSet<>();
-        values.add("value10");
-        values.add("value20");
-        property.setValues(values);
-        propertyMap.put("field2", property);
-        rs.setProperties(propertyMap);
-        rs.setUsernameAttributeProvider(new AnonymousRegisteredServiceUsernameAttributeProvider());
-        
-        return rs;
+    @Override
+    public ServiceRegistry getNewServiceRegistry() {
+        return serviceRegistry;
     }
 }
